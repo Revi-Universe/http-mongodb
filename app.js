db3ae253f355b5c0b910dc36785c1a255bb281d8
Revi-Universe <dldnwn99@gmail.com>
2021-08-07 19:19:33 +0900

    기본 라우팅, html 파일 응답

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const http = require('http');
+const fs = require('fs'); // file system
 
 const server = http.createServer(function(request,response){ 
-
-    response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    response.end('안녕!');
-
+    let url = request.url;
+    console.log(url);
+    if(url == '/'){
+        fs.readFile('index.html', function(error, file) {
+            if(error){
+                response.writeHead(500, {'Content-Type':'text/html; charset=utf-8'});
+                response.end('500 - 서버 내부 에러: '+error);
+            }else{
+                response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                response.end(file);
+            }
+        });
+    }else{
+        response.writeHead(404, {'Content-Type':'text/html'});
+        response.end('404 - 찾을 수 없음');
+    }
 });
 
 server.listen(8080, function(){ 
