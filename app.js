3e5541bda78d7d1c412dd25eaf1f5d9baf35564f
Revi-Universe <dldnwn99@gmail.com>
2021-08-13 23:17:37 +0900

    MongoDB 연결 + POST로 요청 받기

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const fs = require('fs'); // file system
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
 
 const server = http.createServer(function(request,response){ 
     let url = request.url;
-    console.log(url);
+    console.log(`url: ${url}`);
     if(url == '/'){
         fs.readFile('index.html', function(error, file) {
             if(error){
@@ -14,12 +18,28 @@ const server = http.createServer(function(request,response){
                 response.end(file);
             }
         });
+    }else if(url.indexOf('/api/') == 0){
+        request.setEncoding('utf-8'); // 이걸 안해주면 data를 제대로 못읽음!!
+        const api = url.slice(5);
+        request.on('data',(data)=>{
+            switch(api){
+                case 'register':
+                    console.log(data);
+                    response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                    response.end('서버가 일을 안해용~');
+                    break;
+            }
+        });
     }else{
-        response.writeHead(404, {'Content-Type':'text/html'});
+        response.writeHead(404, {'Content-Type':'text/html; charset=utf-8'});
         response.end('404 - 찾을 수 없음');
     }
 });
 
-server.listen(8080, function(){ 
+server.listen(8080, async () => { 
     console.log('웹 서버 실행 중!');
+
+    await client.connect();
+    await client.db('admin').command({ ping: 1 }); // 핑 - 퐁
+    console.log('MongoDB 접속됨!');
 });
